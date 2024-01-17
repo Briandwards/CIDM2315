5af133b613f9ae6bd1e0f873e14eb462048e68db
Briandwards <156940061+Briandwards@users.noreply.github.com>
2024-01-17 15:41:01 -0600

    Add files via upload

---
+++ Homework.cs
@@ -0,0 +1,30 @@
+﻿namespace HelloWorld;
+class Program
+{
+    
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Brian Edwards");
+    }
+}
+using System;
+
+namespace LinearEquationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given values
+            double X = 2.5;
+            double Y = 3.3;
+
+            // Calculate the value of Z using the linear equation
+            double Z = 4 * Math.Pow(X, 2) + 3 * Y;
+
+            // Print the result in the specified format
+            Console.WriteLine("The value of Z for X = {0} and Y = {1} is: {2}", X, Y, Z);
+        }
+    }
+}
