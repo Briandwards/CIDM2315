5c0ae5e1ea8d107f3c21d6df3a55561c8a54bfd7
Briandwards <156940061+Briandwards@users.noreply.github.com>
2024-02-02 15:29:38 -0600

    Add files via upload

---
+++ Homework 3 Q#1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PrimeNumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (IsPrime(N))
+            {
+                Console.WriteLine("N is prime");
+            }
+            else
+            {
+                Console.WriteLine("N is non-prime");
+            }
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i+=2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Homework 3 Q#2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SquarePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(i + 1 + " ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
